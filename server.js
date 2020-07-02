073ef0dadf0049640e35323f65d62bc443203426
mac <mac@macs-MacBook-Pro.local>
2020-07-02 17:46:32 +0200

    fix: deploy

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const express = require('express')
 const app = new express()
 app.use(express.static('build'))
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('App listening on port 3000')
 })
